1d738212f96e848f3c7d80d50b2b2b6081717dbe
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2022-11-29 02:58:49 +0800

    Create ILanguageAnalyzer class

---
+++ PushDownAutomata/PushDownAutomata/Analyzer/ILanguageAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿namespace PushDownAutomata.Analyzer
+{
+	public interface ILanguageAnalyzer
+	{
+		/// <summary>
+		/// Analyzes the input and checks whether it is accepted or not.
+		/// </summary>
+		/// <param name="input"> Input to be analyzed. </param>
+		/// <returns> True or False </returns>
+		bool analyze(string input);
+	}
+}
+
