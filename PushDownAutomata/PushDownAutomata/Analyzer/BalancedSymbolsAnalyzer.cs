6426bcd5a5143b0020188352bb6fd66986c2c363
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2022-11-29 05:44:39 +0800

    Create BalancedSymbolsAnalyzer

---
+++ PushDownAutomata/PushDownAutomata/Analyzer/BalancedSymbolsAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿namespace PushDownAutomata.Analyzer
+{
+	public class BalancedSymbolsAnalyzer : ILanguageAnalyzer
+	{
+        Stack<char> stack = new Stack<char>(); 
+
+        public bool Analyze(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                var current = input[i];
+                if (current == '(' || current == '[' || current == '{')
+                {
+                    stack.Push(current);
+                }
+                else if (current == ')' || current == ']' || current == '}')
+                {
+                    var top = stack.Pop();
+
+                    if (top == '(')
+                    {
+                        if (current != ')')
+                            return false;
+                    }
+                    else if (top == '{')
+                    {
+                        if (current != '}')
+                            return false;
+                    }
+                    else if (top == '[')
+                    {
+                        if (current != ']')
+                            return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            if (stack.Count != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
