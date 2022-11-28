0457aaf62821fc619b6374ae06b51267221ed8c4
CHARIS ARLIE BACLAYON <charisarlie.baclayon@cit.edu>
2022-11-29 05:45:40 +0800

    Create PalindromeAnalyzer

---
+++ PushDownAutomata/PushDownAutomata/Analyzer/PalindromeAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PushDownAutomata.Analyzer
+{
+	public class PalindromeAnalyzer : ILanguageAnalyzer
+	{
+        Stack<char> stack = new Stack<char>();
+
+        public bool Analyze(string input)
+        {
+            if (input.Length % 2 != 0)
+            {
+                input = input.Remove(input.Length / 2, 1);
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                var current = input[i];
+                if (!stack.Contains(current))
+                {
+                    stack.Push(current);
+                }
+                else 
+                {
+                    var top = stack.Pop();
+                    if (top != current)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
